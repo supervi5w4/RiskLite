479ccb91e669f02af2b33cbf474a832340f6b46e
supervi5w4 <supervi5w4@gmail.com>
2025-08-14 21:18:50 +0300

    Add files via upload

--- scenes/map.gd
+++ scenes/map.gd
@@ -3,9 +3,17 @@ class_name Map
 
 signal map_territory_clicked(id: int)
 
-# Подключаем скрипт Territory как ресурс
+# Подключаем скрипт Territory и сцену
 const Territory = preload("res://scenes/territory.gd")
 const TerritoryScene: PackedScene = preload("res://scenes/territory.tscn")
+
+# Константы игроков из GameManager для назначения контроллеров
+const GameManager = preload("res://scenes/game_manager.gd")
+const PLAYER_CYCLE: Array[int] = [
+	GameManager.PLAYER_HUMAN,
+	GameManager.PLAYER_AI_1,
+	GameManager.PLAYER_AI_2,
+]
 
 var territories: Array[Territory] = []
 var adjacency: Dictionary = {}
@@ -21,7 +29,7 @@ func _ready() -> void:
 	for i in range(rows * cols):
 		var territory: Territory = TerritoryScene.instantiate()
 		territory.territory_id = i
-		territory.controller_id = i % 3
+		territory.controller_id = PLAYER_CYCLE[i % PLAYER_CYCLE.size()]
 		territory.start_units = 10
 		territory.allow_clicks = true
 		territory.clicked.connect(_on_territory_clicked)
@@ -55,7 +63,7 @@ func _ready() -> void:
 		adjacency[i] = neighbors
 
 func _on_territory_clicked(id: int) -> void:
-	print("Клик по территории id=", id)
+	print("Клик по территории id=%d" % id)
 	map_territory_clicked.emit(id)
 
 func get_territory_by_id(id: int) -> Territory:
